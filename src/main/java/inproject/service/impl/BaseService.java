aa2762071ce23fa9fbf56843f1bcca4abe65d902
IliaVolk <liailia119@gmail.com>
2017-01-16 09:12:31 +0200

    .

--- src/main/java/inproject/service/impl/BaseService.java
+++ src/main/java/inproject/service/impl/BaseService.java
@@ -1,15 +1,14 @@
 package inproject.service.impl;
 
 import inproject.entity.BaseEntity;
-import inproject.entity.InstagramAuthUser;
 import inproject.service.Service;
 
 abstract public class BaseService<T extends BaseEntity, ID> implements Service<T, ID> {
     @Override
     public T add(T toAdd, String userType, Long userId) {
-        InstagramAuthUser user = new InstagramAuthUser();
+        /*InstagramAuthUser user = new InstagramAuthUser();
         user.setId(userId);
-        toAdd.setUser(user);
+        toAdd.setUser(user);*/
         return add(toAdd);
     }
 }
