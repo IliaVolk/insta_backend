1e1009c7d89febd17039468a711435ac27e89353
IliaVolk <liailia119@gmail.com>
2017-01-17 16:40:58 +0200

    tests

--- src/main/java/inproject/repository/StoreRepository.java
+++ src/main/java/inproject/repository/StoreRepository.java
@@ -1,7 +1,6 @@
 package inproject.repository;
 
 import inproject.entity.Store;
-import inproject.entity.StoreSearchResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,15 +8,12 @@ import org.springframework.data.repository.query.Param;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
     @Query(
-    // "select new inproject.entity.StoreSearchResponse(s, count(s.id))" +
             "select s from Store s join s.tags t where s.place.name = :place " +
                     "and t.name in :tags group by s.id order by count(s.id) desc ")
-    //public Stream<StoreSearchResponse> search(
     public List<Store> search(
             @NotNull@Param("tags")Set<String> tags,
             @NotNull@Param("place") String place);
@@ -27,7 +23,6 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
             @NotNull@Param("place") String place);
 
     @Query(
-            //"select new inproject.entity.StoreSearchResponse(s, count(s.id))" +
             "select s from Store s join s.tags t where t.name in :tags group by s.id order by count(s.id) desc")
     public List<Store> search(@NotNull@Param("tags") Set<String> tags);
 }
